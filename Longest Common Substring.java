767fe7fc4ea5ed087313b58204db3179b4e16fc6
abdul8704 <abdulaziz87049@gmail.com>
2025-07-01 11:52:38 +0530

    stirng, dp

--- Longest Common Substring.java
+++ Longest Common Substring.java
@@ -5,42 +5,38 @@
 // Input: s1 = "ABCDGH", s2 = "ACDGHR"
 // Output: 4
 // Explanation: The longest common substring is "CDGH" with a length of 4.
-
 // Input: s1 = "abc", s2 = "acb"
 // Output: 1
 // Explanation: The longest common substrings are "a", "b", "c" all having length 1.
-
 // Input: s1 = "YZ", s2 = "yz"
 // Output: 0
-
 // Constraints:
 // 1 <= s1.size(), s2.size() <= 103
 // Both strings may contain upper and lower case alphabets.
 
-class Solution {
-    private static boolean check(int i, int j, int R, int C){
-        return (i >= 0 && i < R && j >= 0 && j < C); 
-    }
-    public int longestCommonSubstr(String s1, String s2) {
-        int R = s1.length();
-        int C = s2.length();
-        
-        int dp[][] = new int[R][C];
-        int max = 0;
 
-        for(int row=0; row< R; row++){
-            for(int col=0; col < C; col++){
-                if(s1.charAt(row) == s2.charAt(col)){
-                    if(check(row-1, col-1, R, C)){
-                        dp[row][col] = dp[row-1][col-1] + 1;
-                        
-                    }else{
-                        dp[row][col] = 1;
-                    }
-                    max = Math.max(max, dp[row][col]);
-                }
+
+class Solution {
+    public int longestCommonSubstr(String s1, String s2) {
+        // code here
+        int R = s1.length(), C = s2.length(), res = 0;
+        
+        int dp[][] = new int[R+1][C+1];
+        
+        for(int i=0; i<R; i++){
+            for(int j=0; j<C; j++){
+                if(s1.charAt(i) == s2.charAt(j))
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                
+                else
+                    dp[i+1][j+1] = 0;
+                
+                res = Math.max(res, dp[i+1][j+1]);        
             }
         }
-        return max;
+        
+        return res;
+        
+        
     }
 }
